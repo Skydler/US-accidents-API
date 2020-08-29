e743deb010a607826d1acf5641e85288d6b04868
Almandos, Julian Luis <julianalmandos@gmail.com>
2020-08-29 13:59:15 -0300

    Added endpoints

--- api.js
+++ api.js
@@ -1,13 +1,39 @@
 const express = require("express");
 const accidents = require('./accidents');
-
 const router = express.Router();
 
-router.get('/insert', (req, res) => {
+router.get('/:startTime/:endTime', (req, res) => {
     accidents.insertDocuments();
     res.send({
-        pepe: "hola",
-        edad: 20,
+        tarea: "devolver todos los accidentes ocurridos entre 2 fechas dadas"
+    });
+});
+
+router.get('/mostCommonConditions', (req, res) => {
+    accidents.insertDocuments();
+    res.send({
+        tarea: "determinar las condiciones más comunes en los accidentes (hora del día, condiciones climáticas, etc)"
+    });
+});
+
+router.get('/accidentsWithin/:geoPoint/:radius', (req, res) => {
+    accidents.insertDocuments();
+    res.send({
+        tarea: "dado un punto geográfico y un radio (expresado en kilómetros) devolver todos los accidentes ocurridos dentro del radio."
+    });
+});
+
+router.get('/mostDangerousPoints', (req, res) => {
+    accidents.insertDocuments();
+    res.send({
+        tarea: "devolver los 5 puntos más peligrosos (definiendo un determinado radio)"
+    });
+});
+
+router.get('/:id/averageActionDistance', (req, res) => {
+    accidents.insertDocuments();
+    res.send({
+        tarea: "obtener la distancia promedio desde el inicio al fin del accidente"
     });
 });
 
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
-require('dotenv').config();
 const apiRouter = require('./api');
 
+require('dotenv').config();
 const API_PORT = process.env.API_PORT || 3000;
 
 app.listen(API_PORT, () => {
     console.log(`Server initializating on port ${API_PORT}`);
 });
 
-app.use('/', apiRouter);
+app.use('/accidents', apiRouter);
