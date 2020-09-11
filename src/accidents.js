1dc5166c43034b01d047649ca5cfbbc2ef20ad39
Leonel Mandarino <leonelmandarino@gmail.com>
2020-09-11 19:55:24 -0300

    Refactor bd queries and remove null fields from output

--- src/accidents.js
+++ src/accidents.js
@@ -62,8 +62,8 @@ function findAccidentsWithin({ longitude, latitude }, radius, callback) {
 }
 
 function findMostDangerousPointsWithin({ longitude, latitude }, radius, callback) {
-    const radiusToKm = radius * 1000;
     const coords = [parseFloat(longitude), parseFloat(latitude)]
+    const radiusToKm = radius * 1000;
 
     connection.Accidents.aggregate()
         .near({
@@ -81,11 +81,10 @@ function findMostDangerousPointsWithin({ longitude, latitude }, radius, callback
 }
 
 function findMostCommonWeatherConditions(callback) {
-        const limit = 5;
-    const weatherQuery = [{ $sortByCount: "$Weather_Condition" }, { $limit: limit }, { $project: { _id: false, weather: '$_id', count: true } }]
-    const visibilityQuery = [{ $sortByCount: "$Visibility(mi)" }, { $limit: limit }, { $project: { _id: false, "visibility(mi)": '$_id', count: true } }]
-    const tempQuery = [{ $sortByCount: "$Temperature(F)" }, { $limit: limit }, { $project: { _id: false, "temperature(F)": '$_id', count: true } }]
-    const windQuery = [{ $sortByCount: "$Wind_Direction" }, { $limit: limit }, { $project: { _id: false, windDirection: '$_id', count: true } }]
+    const weatherQuery = constructCommonQuery("Weather_Condition");
+    const visibilityQuery = constructCommonQuery("Visibility");
+    const tempQuery = constructCommonQuery("Temperature");
+    const windQuery = constructCommonQuery("Wind_Direction");
 
     Promise.all([
         connection.Accidents.aggregate(weatherQuery).exec(),
@@ -96,10 +95,9 @@ function findMostCommonWeatherConditions(callback) {
 }
 
 function findMostCommonLocationConditions(callback) {
-    const limit = 5;
-    const timeQuery = [{ $sortByCount: "$Start_Time" }, { $limit: limit }, { $project: { _id: false, time: '$_id', count: true } }]
-    const cityQuery = [{ $sortByCount: "$City" }, { $limit: limit }, { $project: { _id: false, city: '$_id', count: true } }]
-    const stateQuery = [{ $sortByCount: "$State" }, { $limit: limit }, { $project: { _id: false, state: '$_id', count: true } }]
+    const timeQuery = constructCommonQuery("Start_Time");
+    const cityQuery = constructCommonQuery("City");
+    const stateQuery = constructCommonQuery("State");
 
     Promise.all([
         connection.Accidents.aggregate(timeQuery).allowDiskUse(true).exec(),
@@ -109,15 +107,14 @@ function findMostCommonLocationConditions(callback) {
 }
 
 function findMostCommonTerrainConditions(callback) {
-    const limit = 5; //It's all booleans so it's kind of the same here, but I will leave it just in case
-    const crossingQuery = [{ $sortByCount: "$Crossing" }, { $limit: limit }, { $project: { _id: false, crossing: '$_id', count: true } }]
-    const giveAwayQuery = [{ $sortByCount: "$Give_Way" }, { $limit: limit }, { $project: { _id: false, giveAway: '$_id', count: true } }]
-    const junctionQuery = [{ $sortByCount: "$Junction" }, { $limit: limit }, { $project: { _id: false, junction: '$_id', count: true } }]
-    const noExitQuery = [{ $sortByCount: "$No_Exit" }, { $limit: limit }, { $project: { _id: false, noExit: '$_id', count: true } }]
-    const railWayQuery = [{ $sortByCount: "$Railway" }, { $limit: limit }, { $project: { _id: false, railWay: '$_id', count: true } }]
-    const stopQuery = [{ $sortByCount: "$Stop" }, { $limit: limit }, { $project: { _id: false, stop: '$_id', count: true } }]
-    const turningLoopQuery = [{ $sortByCount: "$Turning_Loop" }, { $limit: limit }, { $project: { _id: false, turningLoop: '$_id', count: true } }]
-    const stationQuery = [{ $sortByCount: "$Station" }, { $limit: limit }, { $project: { _id: false, station: '$_id', count: true } }]
+    const crossingQuery = constructCommonQuery("Crossing");
+    const giveAwayQuery = constructCommonQuery("Give_Way");
+    const junctionQuery = constructCommonQuery("Junction");
+    const noExitQuery = constructCommonQuery("No_Exit");
+    const railWayQuery = constructCommonQuery("Railway");
+    const stopQuery = constructCommonQuery("Stop");
+    const turningLoopQuery = constructCommonQuery("Turning_Loop");
+    const stationQuery = constructCommonQuery("Station");
 
     Promise.all([
         connection.Accidents.aggregate(crossingQuery).exec(),
@@ -129,6 +126,15 @@ function findMostCommonTerrainConditions(callback) {
         connection.Accidents.aggregate(turningLoopQuery).exec(),
         connection.Accidents.aggregate(stationQuery).exec(),
     ]).then(values => callback(parser.parseTerrainConditions(values)));
+}
+
+function constructCommonQuery(field, limit = 5) {
+    return [
+        { $sortByCount: `$${field}` },
+        { $match: { _id: { $ne: "" } } },
+        { $limit: limit },
+        { $project: { _id: false, value: '$_id', count: true } }
+    ]
 }
 
 module.exports = {
